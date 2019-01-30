5911ddb0e99a8658898bded153db68f51cbd1ce2
Yilun <zyl.skysniper@gmail.com>
2019-01-30 15:42:44 -0800

    Generate wallet on the first run
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- build_nkn.sh
+++ build_nkn.sh
@@ -14,12 +14,4 @@ sudo -u nkn git checkout ${LATEST_TAG} || exit $?
 sudo -u nkn bash -c "source /home/nkn/.bash_profile && make" || exit $?
 sudo -u nkn bash -c "cp config.testnet.json config.json" || exit $?
 
-RANDOM_PASSWD=$(head -c 1024 /dev/urandom | shasum -a 512 -b | xxd -r -p | base64 | head -c 32) || exit $?
-sudo -u nkn bash -c "./nknc wallet -c" <<EOF
-${RANDOM_PASSWD}
-${RANDOM_PASSWD}
-EOF
 [ $? -eq 0 ] || exit $?
-
-sudo -u nkn bash -c "echo ${RANDOM_PASSWD} > wallet.pswd" || exit $?
-sudo -u nkn bash -c "chmod 0400 wallet.pswd" || exit $?
--- launch_node.sh
+++ launch_node.sh
@@ -6,6 +6,17 @@ ulimit -c unlimited
 source $HOME/.bash_profile
 export GOTRACEBACK=crash
 
+function initWallet () {
+    RANDOM_PASSWD=$(head -c 1024 /dev/urandom | shasum -a 512 -b | xxd -r -p | base64 | head -c 32)
+    ./nknc wallet -c <<EOF
+${RANDOM_PASSWD}
+${RANDOM_PASSWD}
+EOF
+    echo ${RANDOM_PASSWD} > ./wallet.pswd
+    chmod 0400 wallet.pswd
+    return $?
+}
+
 function start () {
     WALLET_PASSWD=$(cat ./wallet.pswd)
     ./nknd --no-nat <<EOF
@@ -20,4 +31,5 @@ git checkout ${LATEST_TAG}
 make
 
 mkdir -p ./Log
+[ -e "wallet.dat" ] || initWallet || ! echo "Init Wallet fail" || exit 1
 start 1>./Log/nohup.out.$(date +%F_%T) 2>&1
